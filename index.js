2c59fd16ef2040b7045fbf2eb27a0004024c5543
Anne-Marie Peard <ampeardeditor@gmail.com>
2021-11-30 19:32:55 +1100

    ha;f done

---
+++ index.js
@@ -0,0 +1,21 @@
+function add(){
+}
+function subtract(){
+}
+function multiply(){
+}
+function divide (){
+}
+
+number = 10;
+
+function add5() {
+return number += 5;
+}
+
+let a = 2;
+let b = 6;
+
+function add(a,b){
+    return a + b;
+}
